de827572082c525ba9549b1f4beab50b2ca57c5e
Xavier Normant <30378214+atlou@users.noreply.github.com>
2024-02-17 11:56:59 -0500

    file restructure

--- views/ContentView.swift
+++ views/ContentView.swift
@@ -32,3 +32,7 @@ struct ContentView: View {
         }
     }
 }
+
+#Preview {
+    ContentView(grid: Grid.shared)
+}
--- views/GridView.swift
+++ views/GridView.swift
@@ -7,50 +7,6 @@
 
 import SwiftUI
 
-struct NodeView: View {
-    @ObservedObject var node: Node
-    let size: Double
-
-    var body: some View {
-        RoundedRectangle(cornerRadius: 0)
-            .fill(.white)
-            .padding(1)
-            .frame(width: size, height: size)
-            .overlay {
-                ZStack {
-                    switch node.type {
-                    case .normal:
-                        switch node.state {
-                        case .base:
-                            Rectangle()
-                                .fill(.clear)
-                        case .visited:
-                            Rectangle()
-                                .fill(.yellow.opacity(0.2))
-                                .transition(.scale.combined(with: .opacity))
-                        case .path:
-                            Rectangle()
-                                .fill(.yellow)
-                                .transition(.opacity)
-                        }
-                    case .wall:
-                        Rectangle()
-                            .fill(.gray.opacity(0.8))
-                            .transition(.scale.combined(with: .opacity))
-                    case .start:
-                        Rectangle()
-                            .fill(.blue)
-                            .transition(.scale.combined(with: .opacity))
-                    case .target:
-                        Rectangle()
-                            .fill(.green.opacity(0.8))
-                            .transition(.scale.combined(with: .opacity))
-                    }
-                }
-            }
-    }
-}
-
 struct GridView: View {
     @ObservedObject var grid: Grid
     let size: Double
